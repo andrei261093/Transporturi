3adbbf1c97eaac583f30dfe1ad1b20ecb6391433
Andrei Iorga <andrei261093@icloud.com>
2016-12-22 15:48:17 +0200

    deviceController addon

--- app/Http/Controllers/DevicesController.php
+++ app/Http/Controllers/DevicesController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Devices;
 
 class DevicesController extends Controller
 {
-    //
+	
+    public function updateOfAddToken(Request $request)
+    {
+       $device = Devices::where('username', $request->username);
+
+       if($device === null){
+       		$new_device = new Devices([
+       			'username' => $request->username;
+       			'token' => $request->fcm_token;
+       		]);
+       		$new_device->save();
+       }else{
+       		$device->token = $request->fcm_token;
+       		$device->save();
+       }
+    }
 }
